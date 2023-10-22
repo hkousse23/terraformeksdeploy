ac2379ade20f21f15798089a71b6135a100080e8
patongit <90884766+patgit12@users.noreply.github.com>
2023-10-22 17:59:07 -0400

    updating

---
+++ users/variables.tf
@@ -0,0 +1,17 @@
+variable "username" {
+  type = list(string)
+  default = [ "Developer1", "manager" ]
+  
+}
+variable "env" {
+  type = list(string) 
+  default = [ "Development", "Production" ] 
+  
+}
+variable "tags" {
+  type = map(string)  
+  default = {
+    "Env" = "Production"
+  }
+  
+}
